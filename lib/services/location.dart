ef6f17e9a4f3e102bf9a3398723c0b654cd66910
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-24 16:43:44 -0400

    better code

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -46,6 +46,25 @@ List<GeoPoint> latlngToGeoPoint(List<LatLng> points) {
   return result;
 }
 
+// void main() {
+//   List<double> logitude = [23, 24, 24];
+//   List<double> Lantiide = [40, 41, 41.5];
+//   List<double> Oldlogitude = [23, 24, 26];
+//   List<double> OldLantiide = [40, 41, 42];
+//   const double time_passed = 8000000;
+//   ai_data cool =
+//   Ai().main(logitude, Lantiide, Oldlogitude, OldLantiide, time_passed);
+//   print(cool.AvgSpeed);
+//   print(cool.speed);
+//   print(cool.distance);
+//   print(cool.extra_data.ahead);
+//   print(cool.extra_data.are_you_ahead);
+//   //196.52626620000004
+//   // 167.69480355000002
+//   // 195228.0
+//   // 145015.0
+//   // false
+// }
 
 class bro_this_return_a_class_use_ahead_and_are_you_ahead {
   late double ahead;
@@ -60,21 +79,26 @@ class ai_data {
 }
 
 class Ai {
-  ai_data main(List<double> logitude, List<double> Lantiide, List<double> Oldlogitude, List<double> OldLantiide, double time) {
-    ai_data return_data = ai_data();
+  ai_data main(List<double> logitude, List<double> Lantiide,
+      List<double> Oldlogitude, List<double> OldLantiide, double time) {
+    ai_data returnData = ai_data();
 
-    return_data.distance = distance(logitude, Lantiide);
+    returnData.distance = distance(logitude, Lantiide);
 
-    return_data.AvgSpeed = speed(return_data.distance, time);
+    returnData.AvgSpeed = speed(returnData.distance, time);
 
-    return_data.extra_data = areYouAhead(return_data.distance, Oldlogitude, OldLantiide);
+    returnData.extra_data =
+        areYouAhead(returnData.distance, Oldlogitude, OldLantiide);
 
-    return_data.speed = speed(
-        Distance()(LatLng(Lantiide[Lantiide.length - 2], logitude[logitude.length - 2]),
-            LatLng(Lantiide[Lantiide.length - 1], logitude[logitude.length - 1])),
+    returnData.speed = speed(
+        const Distance()(
+            LatLng(
+                Lantiide[Lantiide.length - 2], logitude[logitude.length - 2]),
+            LatLng(
+                Lantiide[Lantiide.length - 1], logitude[logitude.length - 1])),
         time / Lantiide.length);
 
-    return return_data;
+    return returnData;
   }
 
   double distance(List<double> logitude, List<double> Lantiide) {
@@ -82,7 +106,9 @@ class Ai {
 
     for (int where = 0; where < logitude.length; where++) {
       try {
-        totalDistance = Distance()(LatLng(Lantiide[where], logitude[where]), LatLng(Lantiide[where + 1], logitude[where + 1])) + totalDistance;
+        totalDistance = const Distance()(LatLng(Lantiide[where], logitude[where]),
+            LatLng(Lantiide[where + 1], logitude[where + 1])) +
+            totalDistance;
       } catch (_) {
         return totalDistance;
       }
@@ -96,25 +122,27 @@ class Ai {
     return first * 2.237 * 3600;
   }
 
-  bro_this_return_a_class_use_ahead_and_are_you_ahead areYouAhead(double newDistance, List<double> Oldlogitude, List<double> OldLantiide) {
-    bro_this_return_a_class_use_ahead_and_are_you_ahead return_data = bro_this_return_a_class_use_ahead_and_are_you_ahead();
+  bro_this_return_a_class_use_ahead_and_are_you_ahead areYouAhead(
+      double newDistance, List<double> Oldlogitude, List<double> OldLantiide) {
+    bro_this_return_a_class_use_ahead_and_are_you_ahead returnData =
+    bro_this_return_a_class_use_ahead_and_are_you_ahead();
 
     double ghost = distance(OldLantiide, Oldlogitude);
 
-    return_data.are_you_ahead = newDistance > ghost;
+    returnData.are_you_ahead = newDistance > ghost;
 
-    switch (return_data.are_you_ahead) {
+    switch (returnData.are_you_ahead) {
       case true:
         {
-          return_data.ahead = newDistance - ghost;
+          returnData.ahead = newDistance - ghost;
         }
         break;
       default:
         {
-          return_data.ahead = ghost - newDistance;
+          returnData.ahead = ghost - newDistance;
         }
         break;
     }
-    return return_data;
+    return returnData;
   }
 }
