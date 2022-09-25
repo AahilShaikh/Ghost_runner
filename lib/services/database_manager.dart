95cc091e86db937d5ec25f72d3ddbb5945e7d603
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-25 00:16:36 -0400

    added new db func and NULL checks bro

--- lib/services/database_manager.dart
+++ lib/services/database_manager.dart
@@ -4,29 +4,56 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:latlong2/latlong.dart';
 
+import '../functions/ai.dart';
+
 class DatabaseManager {
-  static void addNewRunLocation(String locationName, Map<String, dynamic> data, double speed) {
+  static void addNewRunLocation(
+      String locationName, Map<String, dynamic> data, double speed) {
     User user = FirebaseAuth.instance.currentUser!;
     SetOptions options = SetOptions(merge: true);
-    FirebaseFirestore.instance.collection("Users").doc(user.email).collection("RunLocations").doc(locationName).set(data, options);
+    FirebaseFirestore.instance
+        .collection("Users")
+        .doc(user.email)
+        .collection("RunLocations")
+        .doc(locationName)
+        .set(data, options);
     updateSpeed(speed);
   }
 
   static List<String> getRunningLocations() {
     List<String> locations = [];
     User user = FirebaseAuth.instance.currentUser!;
-    FirebaseFirestore.instance.collection("Users").doc(user.email).collection("RunLocations").get().then((value) => {
-          value.docs.forEach((element) {
-            locations.add(element.id);
-          })
-        });
+    FirebaseFirestore.instance
+        .collection("Users")
+        .doc(user.email)
+        .collection("RunLocations")
+        .get()
+        .then((value) => {
+              value.docs.forEach((element) {
+                locations.add(element.id);
+              })
+            });
     return locations;
   }
 
-  static Future<LinkedHashMap<LatLng, int>> getLocationData(String runName) async {
+  static Future<LinkedHashMap<LatLng, int>> getLocationData(
+      String runName) async {
     LinkedHashMap<LatLng, int> result = LinkedHashMap();
-    User user = FirebaseAuth.instance.currentUser!;
-    await FirebaseFirestore.instance.collection("Users").doc(user.email).collection("RunLocations").doc(runName).get().then((value) {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      throw "null error";
+    }
+    String? userEmail = user.email;
+    if (userEmail == null) {
+      throw "null error";
+    }
+    await FirebaseFirestore.instance
+        .collection("Users")
+        .doc(userEmail)
+        .collection("RunLocations")
+        .doc(runName)
+        .get()
+        .then((value) {
       Map<String, dynamic> data = value.data() as Map<String, dynamic>;
       List<dynamic> location = data['Location Data'];
       List<dynamic> time = data['elapsed_time_intervals'];
@@ -39,25 +66,70 @@ class DatabaseManager {
   }
 
   static void updateSpeed(double speed) {
-    User user = FirebaseAuth.instance.currentUser!;
-    SetOptions options = SetOptions(merge: true);
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      throw "null error";
+    }
+    String? userEmail = user.email;
+    if (userEmail == null) {
+      throw "null error";
+    }    SetOptions options = SetOptions(merge: true);
     List<double> speeds;
-    FirebaseFirestore.instance.collection("Users").doc(user.email).get().then((value) {
+    FirebaseFirestore.instance
+        .collection("Users")
+        .doc(userEmail)
+        .get()
+        .then((value) {
       Map<String, dynamic> data = value.data() as Map<String, dynamic>;
       speeds = data['speeds'] ?? [];
       speeds.add(speed);
-      FirebaseFirestore.instance.collection("Users").doc(user.email).set({"speeds": speeds}, options);
+      FirebaseFirestore.instance
+          .collection("Users")
+          .doc(user.email)
+          .set({"speeds": speeds}, options);
     });
   }
 
   static void getSpeeds(double speed) {
-    User user = FirebaseAuth.instance.currentUser!;
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      throw "null error";
+    }
+    String? userEmail = user.email;
+    if (userEmail == null) {
+      throw "null error";
+    }
     List<double> speeds;
-    FirebaseFirestore.instance.collection("Users").doc(user.email).get().then((value) {
+    FirebaseFirestore.instance
+        .collection("Users")
+        .doc(userEmail)
+        .get()
+        .then((value) {
       Map<String, dynamic> data = value.data() as Map<String, dynamic>;
       speeds = data['speeds'] ?? [];
       speeds.add(speed);
       return speeds;
     });
+  }
+
+  static void sendAdvanceRuns(AIData backData) {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      throw "null error";
+    }
+    String? userEmail = user.email;
+    if (userEmail == null) {
+      throw "null error";
+    }
+    SetOptions options = SetOptions(merge: true);
+    List<double> speeds;
+    FirebaseFirestore.instance.collection("Users").doc(user.email).set({
+      "advance_data": {
+        "distance": backData.distance,
+        "avgSpeed": backData.avgSpeed,
+        "ahead_by_how_much": backData.extraData.ahead,
+        "ahead": backData.extraData.areYouAhead,
+      }
+    }, options);
   }
 }
