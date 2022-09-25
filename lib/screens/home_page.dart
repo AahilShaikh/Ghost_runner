5b8f505ef4849ce2d4af4482191cf524f083627c
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-24 21:47:43 -0400

    base graph made

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:wwp_hacks_project/screens/sign_up.dart';
 import 'package:wwp_hacks_project/services/location.dart';
 import 'package:wwp_hacks_project/widgets/fab_button.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,6 +14,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static const double cutOffYValue = 5.0;
+
   final Stream<QuerySnapshot> data =
       FirebaseFirestore.instance.collection('Updates').snapshots();
   List<int>? firestoreData;
@@ -27,8 +30,8 @@ class _HomePageState extends State<HomePage> {
             .doc(email.toString())
             .get()
             .then((everything) {
-            isNull = false;
-            firestoreData?.add(everything["speed"]);
+          isNull = false;
+          firestoreData?.add(everything["speed"]);
           setState(() {});
         });
       } else {
@@ -47,6 +50,12 @@ class _HomePageState extends State<HomePage> {
     checkLocationPermissions(context);
   }
 
+  static const _dateTextStyle = TextStyle(
+    fontSize: 10,
+    color: Colors.purple,
+    fontWeight: FontWeight.bold,
+  );
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -57,7 +66,8 @@ class _HomePageState extends State<HomePage> {
                 color: Theme.of(context).backgroundColor,
                 child: Center(
                   child: CircularProgressIndicator.adaptive(
-                    valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).textTheme.headline1!.color as Color),
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                        Theme.of(context).textTheme.headline1!.color as Color),
                   ),
                 ));
           } else {
@@ -77,14 +87,109 @@ class _HomePageState extends State<HomePage> {
                   tooltip: 'Sign Out',
                   onPressed: () {
                     FirebaseAuth.instance.signOut();
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const SignUpPage()));
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => const SignUpPage()));
                   },
                 ),
               ],
             ),
             body: Center(
               child: Column(
-                children: const [Text("fard")],
+                children: [
+                  AspectRatio(
+                    aspectRatio: 2.4,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 12, right: 24),
+                      child: LineChart(
+                        LineChartData(
+                          lineTouchData: LineTouchData(enabled: false),
+                          lineBarsData: [
+                            LineChartBarData(
+                              spots: const [
+                                FlSpot(0, 4),
+                                FlSpot(1, 3.5),
+                                FlSpot(2, 4.5),
+                                FlSpot(3, 1),
+                                FlSpot(4, 4),
+                                FlSpot(5, 6),
+                                FlSpot(6, 6.5),
+                                FlSpot(7, 6),
+                                FlSpot(8, 4),
+                                FlSpot(9, 6),
+                                FlSpot(10, 6),
+                                FlSpot(11, 7),
+                              ],
+                              isCurved: true,
+                              barWidth: 8,
+                              color: Colors.purpleAccent,
+                              belowBarData: BarAreaData(
+                                show: true,
+                                color: Colors.deepPurple.withOpacity(0.4),
+                                cutOffY: cutOffYValue,
+                                applyCutOffY: true,
+                              ),
+                              aboveBarData: BarAreaData(
+                                show: true,
+                                color: Colors.orange.withOpacity(0.6),
+                                cutOffY: cutOffYValue,
+                                applyCutOffY: true,
+                              ),
+                              dotData: FlDotData(
+                                show: false,
+                              ),
+                            ),
+                          ],
+                          minY: 0,
+                          titlesData: FlTitlesData(
+                            show: true,
+                            topTitles: AxisTitles(
+                              sideTitles: SideTitles(showTitles: false),
+                            ),
+                            rightTitles: AxisTitles(
+                              sideTitles: SideTitles(showTitles: false),
+                            ),
+                            bottomTitles: AxisTitles(
+                              axisNameWidget: const Text(
+                                '2019',
+                                style: _dateTextStyle,
+                              ),
+                              sideTitles: SideTitles(
+                                showTitles: true,
+                                reservedSize: 18,
+                                interval: 1,
+                                getTitlesWidget: bottomTitleWidgets,
+                              ),
+                            ),
+                            leftTitles: AxisTitles(
+                              axisNameSize: 20,
+                              axisNameWidget: const Padding(
+                                padding: EdgeInsets.only(bottom: 8.0),
+                                child: Text('Value'),
+                              ),
+                              sideTitles: SideTitles(
+                                showTitles: true,
+                                interval: 1,
+                                reservedSize: 40,
+                                getTitlesWidget: leftTitleWidgets,
+                              ),
+                            ),
+                          ),
+                          gridData: FlGridData(
+                            show: true,
+                            drawVerticalLine: false,
+                            horizontalInterval: 1,
+                            checkToShowHorizontalLine: (double value) {
+                              return value == 1 ||
+                                  value == 6 ||
+                                  value == 4 ||
+                                  value == 5;
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
             floatingActionButton: const FABBottomSheetButton(),
@@ -92,4 +197,68 @@ class _HomePageState extends State<HomePage> {
           );
         });
   }
+}
+
+Widget bottomTitleWidgets(double value, TitleMeta meta) {
+  String text;
+  switch (value.toInt()) {
+    case 0:
+      text = 'Jan';
+      break;
+    case 1:
+      text = 'Feb';
+      break;
+    case 2:
+      text = 'Mar';
+      break;
+    case 3:
+      text = 'Apr';
+      break;
+    case 4:
+      text = 'May';
+      break;
+    case 5:
+      text = 'Jun';
+      break;
+    case 6:
+      text = 'Jul';
+      break;
+    case 7:
+      text = 'Aug';
+      break;
+    case 8:
+      text = 'Sep';
+      break;
+    case 9:
+      text = 'Oct';
+      break;
+    case 10:
+      text = 'Nov';
+      break;
+    case 11:
+      text = 'Dec';
+      break;
+    default:
+      return Container();
+  }
+
+  return SideTitleWidget(
+    axisSide: meta.axisSide,
+    space: 4,
+    child: Text(text, style: _dateTextStyle),
+  );
+}
+
+const _dateTextStyle = TextStyle(
+  fontSize: 10,
+  color: Colors.purple,
+  fontWeight: FontWeight.bold,
+);
+
+Widget leftTitleWidgets(double value, TitleMeta meta) {
+  const style = TextStyle(color: Colors.black, fontSize: 12.0);
+  return SideTitleWidget(
+    axisSide: meta.axisSide,
+    child: Text('\$ ${value + 0.5}', style: style),
+  );
 }
